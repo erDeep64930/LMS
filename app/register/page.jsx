d0948578345fa9df0fdcef0207b8ba70c7b23301
erDeep640390 <erdeep64930@gmail.com>
2024-05-15 21:18:22 +0530

    form started

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 const Registerpage = () => {
+  const [name,setName]= useState()
+  const [email,setEmail]= useState()
+  const [password,setPassword]= useState()
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // 
+  }
   return (
-    <div>Registerpage</div>
+    <div className='container col-md-4 offset-md-4 pb-5'>
+<form onSubmit={handleSubmit}>
+  <input type="text" className='form-control mb-4 p-4' value={} onChange={}/>
+</form>
+    </div>
   )
 }
 
